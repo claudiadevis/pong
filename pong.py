2434497ef8fe3ef5d81ff6966e4eb5ee6e548e7b
Claudia Devis <claudiagdevis@gmail.com>
2024-10-22 20:46:34 +0200

    pintamos jugadores 1 y 2 y la red

--- pong.py
+++ pong.py
@@ -1,32 +1,82 @@
 import pygame
 
-pygame.init()
-pantalla = pygame.display.set_mode((800, 600))
+ALTO = 600
+ALTO_PALA = 100
+ANCHO = 800
+ANCHO_PALA = 20
+MARGEN = 30
 
-salir = False
-cont = 0
+COLOR_FONDO = (0,0,0) #RGB (red, green, blue)
+COLOR_OBJETOS = (200, 200, 200)
 
-while not salir:
-    #bucle principal (main loop)
-    cont = cont+1
-    print(cont)
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            #print('Se ha cerrado la ventana')
-            salir = True
-        
-        #print('Se ha producido un evento del tipo:', evento)
 
-    # renderizar mis objetos
+class Pong:
 
-    # 1. borrar la pantalla (pìntar un cuadrado del mismo color de fondo del mismo tamaño de la pantalla)
-    pygame.draw.rect(pantalla, (0,0,0), ((0,0), (800,600)))
+    def __init__(self):
+        pygame.init()
+        self.pantalla = pygame.display.set_mode((ANCHO, ALTO))
+        pass
 
-    # 2. pintar los objetos en su nueva posición
-    rectangulo = pygame.Rect(cont/10, cont/5, 300, 150)
-    pygame.draw.rect(pantalla, (cont % 255,68,158), rectangulo)
+    def jugar(self):
+        salir = False
+        cont = 0
 
-    # mostrar los cambios en la pantalla 
-    pygame.display.flip()
+        while not salir:
+            #bucle principal (main loop)
+            cont = cont+1
+            print(cont)
+            for evento in pygame.event.get():
+                if evento.type == pygame.QUIT:
+                    #print('Se ha cerrado la ventana')
+                    salir = True
+                
+                #print('Se ha producido un evento del tipo:', evento)
 
-pygame.quit()
+            # renderizar mis objetos
+
+            # 1. borrar la pantalla (pìntar un cuadrado del mismo color de fondo del mismo tamaño de la pantalla)
+            pygame.draw.rect(self.pantalla, COLOR_FONDO, ((0,0), (ANCHO, ALTO)))
+
+            # 2. pintar jugador 1 (izquierda)
+            # pygame.rect(izq, arriba, ancho, alto)
+            arriba = (ALTO-ALTO_PALA) / 2   #ALTO/2 - ALTO_PALA/2
+            jugador1 = pygame.Rect(MARGEN, arriba, ANCHO_PALA, ALTO_PALA)
+            pygame.draw.rect(self.pantalla, COLOR_OBJETOS, jugador1)
+
+            # 3. pintar jugador 2 (derecha)
+            izquierda = ANCHO - MARGEN - ANCHO_PALA
+            jugador2 = pygame.Rect(izquierda, arriba, ANCHO_PALA, ALTO_PALA)
+            pygame.draw.rect(self.pantalla, COLOR_OBJETOS, jugador2)
+
+            # 4. pintar la red
+            self.pintar_red()
+
+            # mostrar los cambios en la pantalla 
+            pygame.display.flip()
+
+        pygame.quit()
+
+    def pintar_red(self):
+        pos_x = ANCHO / 2
+
+        tramo_pintado = 20
+        tramo_vacio = 15
+        ancho_red = 6
+
+        for y in range(0, ALTO, tramo_pintado + tramo_vacio):
+            pygame.draw.line(
+                self.pantalla, 
+                COLOR_OBJETOS, 
+                (pos_x, y), 
+                (pos_x, y + tramo_pintado), 
+                width = ancho_red)
+            
+
+if __name__ == '__main__':
+    juego = Pong()
+    juego.jugar()
+
+
+
+
+
---
+++ prueba.py
@@ -0,0 +1,9 @@
+from pong import Pong
+
+p = Pong()
+p.jugar()
+
+print('Cambiamos de juego')
+
+q = Pong()
+q.jugar
